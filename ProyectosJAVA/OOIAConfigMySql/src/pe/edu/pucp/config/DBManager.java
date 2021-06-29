a0946e7349dc982a7c22d0ba7d307ef7ec75d632
Dayana31 <70593166+Dayana31@users.noreply.github.com>
2021-06-29 12:52:08 -0500

    DBManager actualizado

--- ProyectosJAVA/OOIAConfigMySql/src/pe/edu/pucp/config/DBManager.java
+++ ProyectosJAVA/OOIAConfigMySql/src/pe/edu/pucp/config/DBManager.java
@@ -1,8 +1,8 @@
 package pe.edu.pucp.config;
 
 public class DBManager{
-    public static String url = "jdbc:mysql://db-proyectolp2.cwoxewmw8gia.us-east-1.rds.amazonaws.com:"
+    public static String url = "jdbc:mysql://databaselp2.clcz5ojgiwk0.us-east-1.rds.amazonaws.com:"
             + "3306/OOIA";//recordar que es el server y luego el schema
     public static String user = "admin";
-    public static String password = "LP22021-1";
+    public static String password = "gato2021";
 }
